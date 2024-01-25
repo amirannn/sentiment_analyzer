9e8bcdcc9d2b690a6852fdc05e90716e3731dc73
amirannn <156032145+amirannn@users.noreply.github.com>
2024-01-25 03:58:40 +0330

    Update Config.php

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -22,7 +22,7 @@ class Config
     //const REGEX_REMOVE_PUNCTUATION = re.compile('[%s]' % re.escape(string.punctuation))
              
     const NEGATE = ["aint", "cannot", "cant", "couldnt", "darent", "didnt", "doesnt",
-        "ain't", "aren't", "can't", "couldn't", "daren't", "didn't", "doesn't",
+        "ain't", "can't", "couldn't", "daren't", "didn't", "doesn't",
         "dont", "hadnt", "hasnt", "havent", "isnt", "mightnt", "mustnt", "neither",
         "don't", "hadn't", "hasn't", "haven't", "isn't", "mightn't", "mustn't",
         "neednt", "needn't", "never", "none", "nope", "nor", "not", "nothing", "nowhere",
